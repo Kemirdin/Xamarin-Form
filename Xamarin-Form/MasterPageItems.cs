fd091f116fa4fbfd8faca79cb5f0aef00e793685
Kemirdin Kaynam <qemirdin@gmail.com>
2018-08-03 23:42:50 +0200

    Xamarin Forms Let’s create MasterPageItems.cs class inside MenuItems folder

---
+++ Xamarin-Form/MasterPageItems.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyNavDraw.MenuItems {
+    public class MasterPageItem {
+        public string Title { get; set; }
+        public string Icon { get; set; }
+        public Type TargetType { get; set; }
+    }
+}
