50912db5cfbc909ece92d723d271ecc2c3bea061
Kemirdin Kaynam <qemirdin@gmail.com>
2018-08-07 22:24:39 +0200

    Xamarin Forms CardView As Like Cards

---
+++ Xamarin-Form/4-CARDVIEW/CardDataModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwesomeCardView.Model {
+    public class CardDataModel {
+        public string HeadTitle { get; set; }
+        public string HeadLines { get; set; }
+        public string HeadLinesDesc { get; set; }
+        public string ProfileImage { get; set; }
+    }
+}
