b1d577e5e7810410480ada0cdc5ea6bf2630c7ec
Kemirdin Kaynam <qemirdin@gmail.com>
2018-08-24 15:31:06 +0200

    Xamarin Forms Shadow Effect

---
+++ Xamarin-Form/21-ShadowEffect/ShaidowEffect.cs
@@ -0,0 +1,11 @@
+	{
+	    public float Radius { get; set; }
+
+	    public Color Color { get; set; }
+
+	    public float DistanceX { get; set; }
+
+	    public float DistanceY { get; set; }
+
+	    public ShadowEffect () : base ("MyCompany.LabelShadowEffect") { }
+	}
