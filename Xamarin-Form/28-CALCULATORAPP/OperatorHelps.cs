bb4b0e8e498f50b3c4a8a2702a880ef63d23b7e7
Kemirdin Kaynam <qemirdin@gmail.com>
2018-08-31 22:52:06 +0200

    asimple implementation of calculator in Xamarin Forms Cross Platform

---
+++ Xamarin-Form/28-CALCULATORAPP/OperatorHelps.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinCalculator {
+    public static class OperatorHelper {
+
+        public static double Calculate (double value1, double value2, string myoperator) {
+            double result = 0;
+            switch (myoperator) {
+                case "÷":
+                    result = value1 / value2;
+                    break;
+                case "*":
+                    result = value1 * value2;
+                    break;
+                case "+":
+                    result = value1 + value2;
+                    break;
+                case "-":
+                    result = value1 - value2;
+                    break;
+
+            }
+            return result;
+
+        }
+
+    }
+
+}
