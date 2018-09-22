fc1ca4a9f491335f0bb07b70eab9825944085e1f
Kemirdin Kaynam <qemirdin@gmail.com>
2018-09-22 22:25:48 +0200

    this is about hoe to add icon for toolbar

---
+++ Xaml/Xaml-Layouts-Examples/ToolbarIconInXamarin/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ToolbarIconExample {
+    public partial class Page1 : ContentPage {
+        public Page1 () {
+            InitializeComponent ();
+            //var cartimg = new ToolbarItem  
+            //    {  
+            //        Icon = "download.png",  
+            //        Priority = 0,  
+            //        Order = ToolbarItemOrder.Primary  
+            //    };  
+        }
+        private void Onclick (object sender, EventArgs e) { }
+    }
+}
