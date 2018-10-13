4c62f61736ce27b6fb5a9ba51d6e54ac34840ef9
Kemirdin Kaynam <qemirdin@gmail.com>
2018-10-13 20:59:18 +0200

    AbsoluteLayout.Layout

---
+++ Xaml/Xaml-Layouts-Examples/AbsoluteLayout/Page2.xaml.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace AbsoluteLayoutDemo {
+    public partial class Example2Page : ContentPage {
+        public Example2Page () {
+            InitializeComponent ();
+        }
+    }
+}
