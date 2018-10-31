81ad7d082b14186e9f554e3bee3f25b67c97954a
Kemirdin Kaynam <qemirdin@gmail.com>
2018-10-31 22:11:07 +0100

    this is about how to create masterdatip page

---
+++ Xaml/Xaml-Layouts-Examples/MDtaiPage/mm.cs
@@ -0,0 +1,19 @@
+namespace Acr.XamForms {
+
+    public class MasterDetailPage : Xamarin.Forms.MasterDetailPage {
+        protected override bool OnBackButtonPressed () {
+            return (this.BindingContext as IViewModelLifecycle)?.OnBack () ?? true;
+        }
+
+        protected override void OnAppearing () {
+            base.OnAppearing ();
+            (this.BindingContext as IViewModelLifecycle)?.OnActivate ();
+        }
+
+        protected override void OnDisappearing () {
+            base.OnDisappearing ();
+            (this.BindingContext as IViewModelLifecycle)?.OnDeactivate ();
+        }
+    }
+}©
+2018 GitHub, Inc.
