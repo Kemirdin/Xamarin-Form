b7075372c36a8ac53a85b21564aab9b91998638b
Kemirdin Kaynam <qemirdin@gmail.com>
2018-11-07 21:09:53 +0100

    slide drower or master list page

---
+++ Xaml/Xaml-Layouts-Examples/SlideDrower/App.cs
@@ -0,0 +1,26 @@
+namespace SliderDrawer {
+    public static class AppProperties {
+        public static int ScreenWidth;
+        public static int ScreenHeight;
+    }
+
+    public partial class App : Application {
+        public App () {
+            InitializeComponent ();
+
+            MainPage = new NavigationPage (new SliderDrawerPage ());
+        }
+
+        protected override void OnStart () {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep () {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume () {
+            // Handle when your app resumes
+        }
+    }
+}
