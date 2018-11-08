eb35145102726299a809ecd17b48b80de1439182
Kemirdin Kaynam <qemirdin@gmail.com>
2018-11-08 23:21:43 +0100

    slide drower or master list page

---
+++ Xaml/Xaml-Layouts-Examples/SlideDrower/MasterPAge.xaml.cs
@@ -0,0 +1,16 @@
+namespace SliderDrawer {
+    public partial class SliderDrawerPage : ContentPage {
+        private MainPageModel mainPageModel;
+        public SliderDrawerPage () {
+            InitializeComponent ();
+            mainPageModel = new MainPageModel ();
+            this.BindingContext = mainPageModel;
+        }
+
+        protected override void OnAppearing () {
+            base.OnAppearing ();
+            mainPageModel.DefaultHeight = App.Current.MainPage.Height;
+            mainPageModel.DefaultWidth = App.Current.MainPage.Width;
+        }
+    }
+}
