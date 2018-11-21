00cbef9ef3b50e83259458f12cb446eeb269e431
Kemirdin Kaynam <qemirdin@gmail.com>
2018-11-21 11:49:27 +0100

    this is about how to create Start time page

---
+++ Xaml/Xaml-Layouts-Examples/TimerPage/StartTimePage.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinTimer {
+    public partial class TimerPage : ContentPage {
+        public TimerPage () {
+            InitializeComponent ();
+        }
+        private void timer (double time) {
+            Device.StartTimer (TimeSpan.FromMinutes (1), () => {
+                time += 1;
+                Label.Text = String.Format ("Travetime :{0}{1}", System.Environment.NewLine, time + "min");
+
+                if (time == 30) {
+                    _lblTime.Text = String.Format ("Yopu are too late bro!!!");
+                    return false;
+                }
+
+                return true;
+            });
+        }
+
+        private void TimerStarterClicked (object sender, EventArgs e) {
+            double time = double.Parse (_EntryTime.Text);
+
+            timer (time);
+        }
+    }
+}
