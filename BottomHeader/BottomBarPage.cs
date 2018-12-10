273d68493a3d740cb20951e2a7a0677e857a2bc6
Kemirdin Kaynam <qemirdin@gmail.com>
2018-12-10 20:58:36 +0100

    BottomNavigationBar for Xamarin Forms

---
+++ BottomHeader/BottomBarPage.cs
@@ -0,0 +1,15 @@
+using System;
+using Xamarin.Forms;
+
+namespace BottomBar.XamarinForms {
+    public class BottomBarPage : TabbedPage {
+        public enum BarThemeTypes { Light, DarkWithAlpha, DarkWithoutAlpha }
+
+        public bool FixedMode { get; set; }
+        public BarThemeTypes BarTheme { get; set; }
+
+        public void RaiseCurrentPageChanged () {
+            OnCurrentPageChanged ();
+        }
+    }
+}
