56d7a37e5f668b745a8f118de7d6553a4c691b77
Kemirdin Kaynam <qemirdin@gmail.com>
2018-12-20 20:36:06 +0100

    master detail page

---
+++ Xaml/Xaml-Layouts-Examples/MasterDPage/MD.xaml.cs
@@ -0,0 +1,8 @@
+namespace DemoApp.MasterDetailPage {
+    [XamlCompilation (XamlCompilationOptions.Compile)]
+    public partial class MasterDetailPageTestDetail : ContentPage {
+        public MasterDetailPageTestDetail () {
+            InitializeComponent ();
+        }
+    }
+}
