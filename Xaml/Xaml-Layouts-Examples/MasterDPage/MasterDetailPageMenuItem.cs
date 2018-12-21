eb87bd3e4308f0b9b5b6ecb14504a61a7a81c424
Kemirdin Kaynam <qemirdin@gmail.com>
2018-12-21 23:24:05 +0100

    master item for mastering page

--- Xaml/Xaml-Layouts-Examples/MasterDPage/MasterDetailPageMenuItem.cs
+++ Xaml/Xaml-Layouts-Examples/MasterDPage/MasterDetailPageMenuItem.cs
@@ -1,1 +1,12 @@
-git 
+namespace DemoApp.MasterDetailPage {
+
+    public class MasterDetailPageTestMenuItem {
+        public MasterDetailPageTestMenuItem () {
+            TargetType = typeof (MasterDetailPageTestDetail);
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public Type TargetType { get; set; }
+    }
+}
