1ab1983cc72541e9bd424c9f18c849827b96484f
Kemirdin Kaynam <qemirdin@gmail.com>
2018-12-26 21:19:16 +0100

    this is about tap page

---
+++ Xaml/Xaml-Layouts-Examples/TabbedPage/TabbedPage.xaml.cs
@@ -0,0 +1,11 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoApp.TabbedPage {
+    [XamlCompilation (XamlCompilationOptions.Compile)]
+    public partial class TabbedPageTest : Xamarin.Forms.TabbedPage {
+        public TabbedPageTest () {
+            InitializeComponent ();
+        }
+    }
+}
