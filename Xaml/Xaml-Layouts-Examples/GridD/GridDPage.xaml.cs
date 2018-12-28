d92aad79e07e0ebcbcacae9ce30856bbecc99c17
Kemirdin Kaynam <qemirdin@gmail.com>
2018-12-28 23:45:21 +0100

    grid page

---
+++ Xaml/Xaml-Layouts-Examples/GridD/GridDPage.xaml.cs
@@ -0,0 +1,11 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoApp {
+    [XamlCompilation (XamlCompilationOptions.Compile)]
+    public partial class GridDemo : ContentPage {
+        public GridDemo () {
+            InitializeComponent ();
+        }
+    }
+}
