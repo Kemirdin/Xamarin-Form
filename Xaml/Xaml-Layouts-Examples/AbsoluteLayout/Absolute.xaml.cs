059f81f1f5573c1c0410a3a798fd0b0bd54cce62
Kemirdin Kaynam <qemirdin@gmail.com>
2018-12-30 00:02:58 +0100

    code behind

---
+++ Xaml/Xaml-Layouts-Examples/AbsoluteLayout/Absolute.xaml.cs
@@ -0,0 +1,8 @@
+namespace DemoApp {
+    [XamlCompilation (XamlCompilationOptions.Compile)]
+    public partial class AbsoluteLayoutDemo : ContentPage {
+        public AbsoluteLayoutDemo () {
+            InitializeComponent ();
+        }
+    }
+}
