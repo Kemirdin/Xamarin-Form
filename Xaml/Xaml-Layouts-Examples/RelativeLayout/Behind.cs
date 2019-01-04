eb9d23c924f0d7c4b16e583c14b48e8401e14630
Kemirdin Kaynam <qemirdin@gmail.com>
2019-01-04 22:15:45 +0100

    this is about realaytive layout using xaml behind

---
+++ Xaml/Xaml-Layouts-Examples/RelativeLayout/Behind.cs
@@ -0,0 +1,11 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoApp {
+    [XamlCompilation (XamlCompilationOptions.Compile)]
+    public partial class RelativeLayoutDemo : ContentPage {
+        public RelativeLayoutDemo () {
+            InitializeComponent ();
+        }
+    }
+}
