0ec76a5c813b23895e8c164b9b32d8adccb722c0
Kemirdin Kaynam <qemirdin@gmail.com>
2019-01-11 21:17:14 +0100

    Inter face for media service

---
+++ SelectMultiIpleImages/Services/IMediaService.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SelectMultiIpleImagesApp.Services {
+    public interface IMediaService {
+        Task OpenGallery ();
+        void ClearFiles (List<string> filePaths);
+    }
+}
