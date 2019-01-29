2ff5effa08ecaca506145fc404f477e1e33dfa4f
Kemirdin Kaynam <qemirdin@gmail.com>
2019-01-29 22:36:34 +0100

    renderers using for IOS platforms

---
+++ CrossRenderers/IOSDrawViewRenderer.cs
@@ -0,0 +1,28 @@
+using Xamarin.Forms.Platform.iOS;
+
+[assembly : ExportRenderer (typeof (DrawView), typeof (DrawViewRenderer))]
+namespace Renderers.iOS {
+    public class DrawViewRenderer : ViewRenderer<DrawView, NativeDrawView> {
+
+        UIView View { get; set; }
+        protected override void OnElementChanged (ElementChangedEventArgs<DrawView> e) {
+            base.OnElementChanged (e);
+
+            if (Control == null) {
+                SetNativeControl (new NativeDrawView ());
+            }
+
+            if (e.NewElement != null) {
+                Control.PaintColor = e.NewElement.DrawColor.ToUIColor ();
+            }
+        }
+
+        protected override void OnElementPropertyChanged (object sender, System.ComponentModel.PropertyChangedEventArgs e) {
+            base.OnElementPropertyChanged (sender, e);
+
+            if (e.PropertyName == DrawView.DrawColorProperty.PropertyName)
+                Control.PaintColor = Element.DrawColor.ToUIColor ();
+        }
+
+    }
+}
