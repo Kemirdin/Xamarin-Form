8a142d0ba0ed7ac4e34438dd56e7e0f0210fed0f
Kemirdin Kaynam <qemirdin@gmail.com>
2019-02-02 22:54:55 +0100

    C sharp  of font

---
+++ FontPage/FontCodeBehinde.cs
@@ -0,0 +1,13 @@
+using Xamarin.Forms;
+
+namespace FontSample {
+    public partial class FontSamplePage : ContentPage {
+        public FontSamplePage () {
+            InitializeComponent ();
+        }
+
+        async void Handle_Clicked (object sender, System.EventArgs e) {
+            await Navigation.PushModalAsync (new HomePage ());
+        }
+    }
+}
