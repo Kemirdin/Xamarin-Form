e99fd089b31889885dbb5a412f2e5b4540c6618d
Kemirdin Kaynam <qemirdin@gmail.com>
2019-02-17 20:39:59 +0100

    code bejinde

---
+++ MasterDT(Youtube)/CystomerIndicator.xaml.cs
@@ -0,0 +1,7 @@
+ public partial class CustomIndicator : ContentView {
+     public CustomIndicator () {
+         InitializeComponent ();
+         this.webView.Source = new HtmlWebViewSource { Html = "<html><body><img src='loading.gif' style='position: absolute; top: 0; left: 0; right: 0; bottom: 50; margin: auto;' /></body></html>" };
+     }
+ }
+ }
