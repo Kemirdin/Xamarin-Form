f76f034e7109085489bfc726e9f447756615c091
Kemirdin Kaynam <qemirdin@gmail.com>
2019-05-06 21:50:32 +0200

    swicher page

---
+++ CustomLayouts/SwicherPage.cs
@@ -0,0 +1,37 @@
+using System;
+using CustomLayouts.Controls;
+using Xamarin.Forms;
+
+namespace CustomLayouts {
+    public class SwitcherPage : ContentPage {
+        public SwitcherPage () {
+            Title = "Pager Layout w/ Indicators";
+
+            var none = new Button {
+                HorizontalOptions = LayoutOptions.Center,
+                Text = "No pager indicator",
+                Command = new Command (async (obj) => await Navigation.PushAsync (new HomePage (CarouselLayout.IndicatorStyleEnum.None)))
+            };
+            var dots = new Button {
+                HorizontalOptions = LayoutOptions.Center,
+                Text = "Dots",
+                Command = new Command (async (obj) => await Navigation.PushAsync (new HomePage (CarouselLayout.IndicatorStyleEnum.Dots)))
+            };
+            var tabs = new Button {
+                HorizontalOptions = LayoutOptions.Center,
+                Text = "Tabs",
+                Command = new Command (async (obj) => await Navigation.PushAsync (new HomePage (CarouselLayout.IndicatorStyleEnum.Tabs)))
+            };
+            Content = new StackLayout {
+                Orientation = StackOrientation.Vertical,
+                VerticalOptions = LayoutOptions.Center,
+                Spacing = 20,
+                Children = {
+                none,
+                dots,
+                tabs
+                }
+            };
+        }
+    }
+}
